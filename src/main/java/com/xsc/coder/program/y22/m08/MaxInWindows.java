20cd6c73357f722d00f192e2e2cb2cf837375013
夏 <1635585052@qq.com>
2022-09-16 00:20:52 +0800

    滑动窗口最大值

--- src/main/java/com/xsc/coder/program/y22/m08/MaxInWindows.java
+++ src/main/java/com/xsc/coder/program/y22/m08/MaxInWindows.java
@@ -38,9 +38,88 @@ import java.util.ArrayList;
  */
 public class MaxInWindows {
 
-    public ArrayList<Integer> maxInWindows(int[] num, int size) {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,2,6,2,5,1};
+        System.out.println(maxInWindows(nums, 3).toString());
+    }
 
-        return null;
+    public static ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> result = new ArrayList<>();
+        // 初始化一个比较的数组
+        int[] newNums = new int[size];
+        for (int i = 0; i <= num.length - size; i++) {
+            if (i == 0) {
+                System.arraycopy(num, i, newNums, 0, size);
+                heap(newNums);
+            } else {
+                int delIndex = getIndex(newNums, num[i - 1]);
+                int deleteVal = newNums[delIndex];
+                int newVal = num[i + size - 1];
+                if (newVal <= deleteVal) {
+
+                } else if (newVal > deleteVal && newVal <= )
+                newNums[delIndex] = num[i + size - 1];
+
+            }
+            if (newNums.length > 0) {
+                result.add(newNums[0]);
+            }
+        }
+        return result;
+    }
+
+    private static int getIndex(int[] nums, int v) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == v) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 把数组转换成大顶推
+     *
+     * @param nums 原始数组
+     */
+    private static void heap(int[] nums) {
+        for (int i = nums.length / 2; i >= 0; i--) {
+            sort(nums, i);
+        }
+    }
+
+    /**
+     * 把数组i号位置上的值和左右节点上的值进行比较，如果左右节点上的值比它大，则交换，并递归进行
+     *
+     * @param nums 数组
+     * @param i    i好位置
+     */
+    private static void sort(int[] nums, int i) {
+        int index = i;
+        int len = nums.length;
+        if (2 * i + 1 < len && nums[2 * i + 1] > nums[index]) {
+            index = 2 * i + 1;
+        }
+        if (2 * i + 2 < len && nums[2 * i + 2] > nums[index]) {
+            index = 2 * i + 2;
+        }
+        if (index != i) {
+            swap(nums, i, index);
+            sort(nums, index);
+        }
+    }
+
+    /**
+     * 数组值交换
+     *
+     * @param nums 原始数组
+     * @param i    第i位置上的值
+     * @param j    第j位置上的值
+     */
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
 }
